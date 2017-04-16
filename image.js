a8a6498627b76466f0951165a87165fae2d013d0
portiaherndon <portia.d.herndon@bison.howard.edu>
2017-04-16 04:30:45 +0000

    current status

--- image.js
+++ image.js
@@ -1,12 +1,12 @@
 var add = $("add");
 //var delete = $("delete");
 
-window.onload = function addImage() {
+function addImage() {
 	var url=document.getElementById('pic').value;
 	var img=document.createElement("img");
 	var div=document.createElement('planning'); 
 	img.src = url;
-	
+	//document.getElementById('planning').innerHTML=img.src;
 	div.appendChild(img);
 	document.getElementById('planning').appendChild(div);
 		
