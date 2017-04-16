2e478a699579a09617a021d515a2ff53ad7ba711
portiaherndon <portia.d.herndon@bison.howard.edu>
2017-04-16 02:00:49 +0000

    trying to figure out how to print the image onto the html page

---
+++ image.js
@@ -0,0 +1,8 @@
+var add = $("add");
+//var delete = $("delete");
+
+function addImage() {
+	var img = document.createElement("img");
+	this.src = document.getElementById("pic");
+	document.body.appendChild(img);	
+}
