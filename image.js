3c709fc2e08daff93ea0c5c0f90e0313418d7b6f
portiaherndon <portia.d.herndon@bison.howard.edu>
2017-04-16 19:32:02 +0000

    been working since 1. trying to get the image to load onto the page. However, there is a quick load but only for a few seconds.

--- image.js
+++ image.js
@@ -3,11 +3,9 @@ var add = $("add");
 
 function addImage() {
 	var url=document.getElementById('pic').value;
-	var img=document.createElement("img");
-	var div=document.createElement('planning'); 
-	img.src = url;
-	//document.getElementById('planning').innerHTML=img.src;
-	div.appendChild(img);
-	document.getElementById('planning').appendChild(div);
-		
+	var img=document.createElement("img"); 
+	var div=document.getElementById("planning"); 
+	img.src=url;
+	document.getElementById("planning").innerHTML=img.src; 
+	div.appendChild(img); 
 }
