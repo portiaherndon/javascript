fde103e88940da2cb2885dd0fbfe95aac7782a80
portiaherndon <portia.d.herndon@bison.howard.edu>
2017-04-19 04:09:06 +0000

    current status

--- image.js
+++ image.js
@@ -1,8 +1,12 @@
+/*addImage function adds the pic to the planning area.
+It checks to make sure the image does not already exist. If it does
+it does not add it. */
 function addImage() {
 	var url=document.getElementById('pic').value;
 	var img=document.createElement("img"); 
 	var div=document.getElementById("planning"); 
 	div.className="pictures"; 
+	img.className="pics";
 	img.src=url; 
 	img.onclick=function() {urlFunction(this)}; 
 	var pics = div.childNodes; 
@@ -16,9 +20,15 @@ function addImage() {
 		{
 			div.appendChild(img); 
 		}
+		else 
+			//$("error").innerHTML = "This image already exists";	
+			alert("This image already exists");
+			
 	} 
 	return false; 
 }
+/*This function deletes the image requested by the user. If the image does exist
+it shoots an error message to the user to let them know*/
 function deleteImage()
 {
 	var url = document.getElementById('pic').value;
@@ -35,9 +45,17 @@ function deleteImage()
 			}
 		}		
 	}
+	else
+	{
+		//$("error").innerHTML = "This image does not exist";
+		alert("This image does not exist");
+		setTimeout(fade(),5000);	
+	}
 	return false;	
 }
-
+/*the search function is used by addImage and deleteImage to check for the existence
+of the image requested. If the image is found in the planning area, it returns true.
+if not, it returns false. */
 function search(source1,source2)
 { 
 	for(var i=0;i<=source1.length-1;i++)
@@ -49,6 +67,7 @@ function search(source1,source2)
 	} 
 	return false; 
 }
+/*this function gets the src of the image when clicked and places it into the textbox*/
 function urlFunction(element)
 {
 	var url = element.src;
